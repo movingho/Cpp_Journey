7589960c088ae91402342bc294a65c70811e9ca6
movingho <126740002+movingho@users.noreply.github.com>
2025-07-15 00:10:32 +0900

    Create 8393_2.cpp

---
+++ Baekjoon/8393_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	int inputNum, sum = 0;
+
+	cin >> inputNum;
+
+	sum = inputNum * (inputNum + 1) / 2;
+
+	cout << sum << endl;
+
+
+
+}
