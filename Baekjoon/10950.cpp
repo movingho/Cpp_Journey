2e5de62b23137b118cec3c10952de18c767bbcfb
movingho <126740002+movingho@users.noreply.github.com>
2025-07-15 00:00:34 +0900

    Create 10950.cpp

---
+++ Baekjoon/10950.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	int testCase, num1, num2;
+
+	cin >> testCase;
+	
+
+	for (int i = 0; i < testCase; i++)
+	{
+		cin >> num1 >> num2;
+		cout << num1 + num2 << endl;
+	}
+
+}
